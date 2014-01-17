402ce21ce2545dd372f624079af4ee9ebb3237c5
Tammam Kbeili <tammam.kbeili@gmail.com>
2014-01-17 15:51:15 -0800

    implement default route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ AwesomeAnswers::Application.routes.draw do
 
   resources :questions
 
+  root 'questions#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
