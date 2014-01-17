718e0de56aff0f5b03a9beb803ee19c752ca3c5c
Tammam Kbeili <tammam.kbeili@gmail.com>
2014-01-17 14:48:26 -0800

    integrate bootstrap and improve look and feel

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,21 @@
 class QuestionsController < ApplicationController
+
   def index
+    @questions = Question.all
   end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    question = Question.new params.require(:question).permit([:title, :body])
+    question.save
+    redirect_to questions_path
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,9 @@
 class Question < ActiveRecord::Base
+
+  before_save :capialize_title
+
+
+  def capitalize_title
+    self.title = title.capitalize
+  end
 end
